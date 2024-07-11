3f98a5f788f4dbbe7722cd052959537d9486796e
Valerius Mattfeld <mail@valerius.me>
2024-07-11 16:04:38 +0200

    create redirect to webtop script

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+    return {"greeting": "Hello there!"}
+
+
+@app.get("/webtop")
+async def webtop():
+    redirect_url = ("https://gist.githubusercontent.com/valerius21/40d9055856e02f00acdb809ea3b36b5b/raw/acd2f1bae"
+                    "1a2212"
+                    "668e9a61a0dd10d0e5924e38c/webtop.sh")
+    return RedirectResponse(redirect_url)
